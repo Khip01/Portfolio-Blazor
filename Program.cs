fb00837f72e8637b89ada08b33d3896febf6edf3
Khip01 <aakhifathallah24@gmail.com>
2023-07-12 20:08:51 +0700

    Add Blazor WASM

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using Khip01_PortfolioV1_Blazor.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services; // MudBlazor Add Services
+
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#LoadMasterLayout");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddMudServices(); // MudBlazor Add Services
+
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
